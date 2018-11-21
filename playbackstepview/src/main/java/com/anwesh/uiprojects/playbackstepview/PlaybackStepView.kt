80f9cbf010e8799fbffa4c9cbd73e9d660a9996f
Anwesh43 <anweshthecool0@gmail.com>
2018-11-21 14:36:54 +0530

    adding libraries and defining nodes

--- playbackstepview/src/main/java/com/anwesh/uiprojects/playbackstepview/PlaybackStepView.kt
+++ playbackstepview/src/main/java/com/anwesh/uiprojects/playbackstepview/PlaybackStepView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.playbackstepview
 /**
  * Created by anweshmishra on 21/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+
+val BACK_COLOR : Int = Color.parseColor("#212121")
+
+val COLOR : Int = Color.parseColor("#EEEEEE")
