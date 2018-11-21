4f01094b14c8cecfa245782687eb62491046248e
Anwesh43 <anweshthecool0@gmail.com>
2018-11-21 15:19:21 +0530

    adding PlaybackStep class

--- playbackstepview/src/main/java/com/anwesh/uiprojects/playbackstepview/PlaybackStepView.kt
+++ playbackstepview/src/main/java/com/anwesh/uiprojects/playbackstepview/PlaybackStepView.kt
@@ -161,4 +161,28 @@ class PlaybackStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class PlaybackStep(var i : Int, val state : State = State()) {
+
+        private var curr : PSNode = PSNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        } 
+    }
 }
