d885b9677a5e41502db169f3c4e08890bd9d9c49
Anwesh43 <anweshthecool0@gmail.com>
2018-11-21 15:25:28 +0530

    adding PlaybackStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedplaybackstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedplaybackstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedplaybackstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.playbackstepview.PlaybackStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PlaybackStepView.create(this)
     }
 }
